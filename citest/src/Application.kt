0521d15e753ad0b295b3c7c637be84d1768e4b47
tiger <tiger.fdev@gmail.com>
2019-09-24 21:37:15 +0900

    add citest Dockerfile

---
+++ citest/src/Application.kt
@@ -0,0 +1,19 @@
+package citest
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/hello") {
+            call.respondText("Hello World")
+        }
+    }
+}
