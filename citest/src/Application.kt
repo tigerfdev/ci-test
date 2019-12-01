c772ceb43c488c0caa857ee61df8d54907e8c2be
tiger <tiger.fdev@gmail.com>
2019-12-01 22:25:06 +0900

    ci test

--- citest/src/Application.kt
+++ citest/src/Application.kt
@@ -5,15 +5,17 @@ import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import java.time.LocalDateTime
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+var date = LocalDateTime.now()
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
-            call.respondText("Hello World")
+            call.respondText("Hello World $date")
         }
     }
 }
