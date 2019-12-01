4a7bb995fbd3b2f45727565c1509823e3c20cee9
tiger <tiger.fdev@gmail.com>
2019-12-01 15:01:04 +0900

    ci test

--- citest/src/Application.kt
+++ citest/src/Application.kt
@@ -13,7 +13,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     routing {
         get("/") {
-            call.respondText("Hello World s")
+            call.respondText("Hello World yatta")
         }
     }
 }
